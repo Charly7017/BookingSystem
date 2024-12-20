fbda838ba9b4b781302be7565ac14842d004bbff
Carlos Santamaria <alexrace18@gmail.com>
2024-12-19 22:14:20 -0600

    Se termino la funcionalidad de crear villa

--- BookingSystem/Controllers/VillaController.cs
+++ BookingSystem/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using BookingSystem.Infrastructure.Data;
+﻿using BookingSystem.Domain.Entities;
+using BookingSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingSystem.Web.Controllers
@@ -12,15 +13,45 @@ namespace BookingSystem.Web.Controllers
             _db = db;
         }
 
-
-
-
         public IActionResult Index()
         {
 
             var villas = _db.Villas.ToList();
 
             return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(villa);
+            }
+
+            _db.Villas.Add(villa);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            var obj = _db.Villas.FirstOrDefault(p=>p.Id == villaId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
         }
     }
 }
